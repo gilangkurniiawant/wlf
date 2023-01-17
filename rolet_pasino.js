829933762c48e6a5e6a83770d91a8925c3fdf668
gilangkurniiawant <gkurniiawant@gmail.com>
2023-01-17 07:47:47 +0700

    Update rolet_pasino.js

--- rolet_pasino.js
+++ rolet_pasino.js
@@ -32,7 +32,7 @@ console.log(jum_sesi);
         await delay(50);
     }
 
-    for (let index = 0; index < 30; index++) {
+    for (let index = 0; index < 5; index++) {
         randomseed();
 
     }
