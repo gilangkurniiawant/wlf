4150ee82c5048e815b3b9a938ce1181152289169
gilangkurniiawant <gkurniiawant@gmail.com>
2023-01-23 04:25:38 +0700

    Update wolf.js

--- wolf.js
+++ wolf.js
@@ -124,7 +124,7 @@ async function bet(cnom) {
                 },
                 async function(e, r, body) {
                     all_exc++;
-                    if (all_exc > 2500) {
+                    if (all_exc > 1000) {
                         await get_largebet();
 
                         if (bet_besar > lb) {
