a38eb4c31ae505ce7d398a6659cd17b980027f9c
gilangkurniiawant <gkurniiawant@gmail.com>
2023-01-23 07:55:25 +0700

    Update wolf.js

--- wolf.js
+++ wolf.js
@@ -150,7 +150,7 @@ async function bet(cnom) {
                                 }
                                 await delay(1500);
                             }
-                            if (body.bet.amount > 2) {
+                            if (body.bet.amount > 0.6) {
                                 await tele("Bet Dihentikan " + body.bet.amount + " https://wolf.bet/user/transactions?betType=dice&id=" + body.bet.hash + "&modal=bet | Session : https://wolf.bet/user/transactions?betType=session&id=" + data_sesi[cnom] + "&modal=session&table=sessions");
                                 await stop_sesi(cnom);
                                 await get_sesi(cnom);
